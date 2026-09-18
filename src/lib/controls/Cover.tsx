26e7790c94ec7c51e6b2d2435efb5f3e54dfdf95
Oleg <olegus@Olegs-MBP.lan>
2026-09-18 10:07:25 +0300

    feat(Cover): improve placeholder display

--- src/lib/controls/Cover.tsx
+++ src/lib/controls/Cover.tsx
@@ -1,16 +1,15 @@
 import { FC } from 'react';
-import { cssClassNames } from '../utils';
 import { useNedPlayerContext } from '../NedPlayerContext';
 
 const Cover: FC = () => {
     const { currentTrack } = useNedPlayerContext();
 
     return (
-        <div className={cssClassNames('ned-player__cover', {
-            ['ned-player__cover--placeholder']: !currentTrack?.coverSrc
-        })}>
-            {currentTrack?.coverSrc && (
+        <div className='ned-player__cover'>
+            {currentTrack?.coverSrc ? (
                 <img draggable={false} src={currentTrack.coverSrc} />
+            ) : (
+                <div className='ned-player__cover-placeholder'>?</div>
             )}
         </div>
     );
